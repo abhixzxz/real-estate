6aed4031ad642036fb4c669c3ae3bc31330b6adc
Abiraj k <abhi@Abirajs-MacBook-Air.local>
2023-10-18 18:17:59 +0530

    fix ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 // import React from 'react'
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/search" element={<Search />} /> */}
+        {/* <Route path="/listing/:listingId" element={<Listing />} /> */}
+
+        {/* <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
