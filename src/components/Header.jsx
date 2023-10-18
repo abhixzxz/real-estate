f85e62698a2b75e3ad1a0ee0dafde2543f1a770d
Abiraj k <abhi@Abirajs-MacBook-Air.local>
2023-10-18 19:38:00 +0530

    fix ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+        <span className="text-slate-500">Sahand</span>
+        <span className="text-slate-700">Estate</span>
+      </h1>
+      {/* <form>
+        <input type="search" placeholder="search.." />
+      </form> */}
+    </header>
+  );
 }
 
 export default Header;
