3ebef88d6dd1c6bd87ab739a53a52ba296d14ef7
Abiraj k <abhi@Abirajs-MacBook-Air.local>
2023-10-19 15:08:51 +0530

    set up and connected the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log(`Server is running on 3000`);
 });
