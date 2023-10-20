7ab5df7d9ab64875d67afefcef574f7a00c42beb
Abiraj k <abhi@Abirajs-MacBook-Air.local>
2023-10-20 09:54:48 +0530

    set up signin page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,8 +8,8 @@ function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Abhi</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500">Family</span>
+            <span className="text-slate-700">Fortune</span>
           </h1>
         </Link>
 
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function SignIn() {
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg items-center mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Sign-Up</h1>
+      <form className="flex flex-col gap-3 ">
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded lg"
+          id="password"
+        />
+        <button className="bg-slate-700 text-slate-50 p-3 rounded uppercase hover:opacity-90 disabled:opacity-5">
+          Sign-Up
+        </button>
+      </form>
+      <div className="flex gap-3">
+        <p className="">Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500"> Sign in</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default SignIn
+export default SignIn;
